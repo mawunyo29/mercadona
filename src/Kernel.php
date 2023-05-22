1253061bb0ea54691fdab5b69aadae3f7578617e
mawunyo29 <mawunyo73@hotmail.com>
2023-05-22 21:25:46 +0200

    change Procfile congig to web: heroku-php-apache2 public web/

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,17 +9,4 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-
-    // public function registerBundles(): iterable
-    // {
-    //     $contents = require $this->getProjectDir().'/config/bundles.php';
-    //     foreach ($contents as $class => $envs) {
-    //         if (isset($envs['all']) || isset($envs[$this->getEnvironment()])) {
-    //             yield new $class();
-    //         }
-    //     }
-
-    // }
-    
-    
 }
